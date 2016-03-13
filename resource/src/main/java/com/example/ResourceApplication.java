424a65868091203b07797424c3e871064c97f6f3
ming42889 <ming42889@hotmail.com>
2016-03-13 19:21:19 -0400

    part5

--- resource/src/main/java/com/example/ResourceApplication.java
+++ resource/src/main/java/com/example/ResourceApplication.java
@@ -6,18 +6,16 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableRedisHttpSession
+@EnableResourceServer
 @EnableWebSecurity
-public class ResourceApplication extends WebSecurityConfigurerAdapter {
+public class ResourceApplication {
 
 	@RequestMapping("/")
 	public Map<String, Object> home() {
@@ -29,16 +27,5 @@ public class ResourceApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceApplication.class, args);
-	}
-
-	// @Bean
-	// public HeaderHttpSessionStrategy sessionStrategy() {
-	// return new HeaderHttpSessionStrategy();
-	// }
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().disable();
-		http.authorizeRequests().anyRequest().authenticated();
 	}
 }
